07e28e760a7118016f68a5a6968c32be6f9533a0
Taywony <109189690+Taywony@users.noreply.github.com>
2022-08-11 23:46:24 +0900

    Time: 162 ms (19.58%), Space: 45.1 MB (59.60%) - LeetHub

--- 136-single-number/136-single-number.js
+++ 136-single-number/136-single-number.js
@@ -4,17 +4,17 @@
  */
 var singleNumber = function(nums) {
     
-let result = 0;
-let temp = nums.sort((a,b) => a-b);
-
-for(let i = 0; i < temp.length; i++) {
-  result += temp[i];
-}
-
-for(let i = 1; i < temp.length; i ++) {
-  if(temp[i-1] == temp[i]) { 
-    result -= (temp[i] * 2);
-  }
-}
+    let result = 0;
+    let temp = nums.sort((a,b) => a-b);
+    
+    for(let i = 0; i < temp.length; i++) {
+        result += temp[i];
+    }
+    
+    for(let i = 1; i < temp.length; i ++) {
+        if(temp[i-1] == temp[i]) { 
+            result -= (temp[i] * 2);
+        }
+    }
     return result;
 };

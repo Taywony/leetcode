b0800376454958c2aec6581e98b2594362fa694b
Taywony <109189690+Taywony@users.noreply.github.com>
2022-08-12 00:08:02 +0900

    Time: 179 ms (28.89%), Space: 42.1 MB (86.84%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -5,15 +5,6 @@
  */
 var twoSum = function(nums, target) {
     
-    let array = [];
-    
-    for(let i = 0; i < nums.length; i++){
-        if (target >= nums[i]) {
-            array.push(nums[i]);
-            console.log(array)
-        }
-    }
-    
     for (let i = 0; i < nums.length; i++) {
         for (let j = i+1; j < nums.length; j++) {
             if( target == nums[i] + nums[j]) return [i,j];
